a726ced945873e3911b066c173e2c5f8585fdb89
surendrakumarreddy79@gmail.com <surendrayenika@gmail.com>
2026-01-14 16:24:16 +0530

    ready to deploy .,

--- gigflow-frontend/src/App.jsx
+++ gigflow-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -6,49 +6,72 @@ import Gigs from "./pages/Gigs";
 import CreateGig from "./pages/CreateGig";
 import Bids from "./pages/Bids";
 import ProtectedRoute from "./ProtectedRoute";
- import MyBids from "./pages/MyBids";
- import MyPostedGigs from "./pages/MyPostedGigs";
+import MyBids from "./pages/MyBids";
+import MyPostedGigs from "./pages/MyPostedGigs";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 const App = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <>
       <Navbar />
+
       <Routes>
+        {/* Home */}
         <Route path="/" element={<Gigs />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
 
-        <Route path="/create-gig" element={
-          <ProtectedRoute><CreateGig/></ProtectedRoute>
-        }/>
+        {/* Auth Pages */}
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" replace />}
+        />
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/" replace />}
+        />
 
-        <Route path="/bids/:gigId" element={
-          <ProtectedRoute><Bids/></ProtectedRoute>
-        }/>
+        {/* Protected Pages */}
+        <Route
+          path="/create-gig"
+          element={
+            <ProtectedRoute>
+              <CreateGig />
+            </ProtectedRoute>
+          }
+        />
 
         <Route
-  path="/my-gigs"
-  element={
-    <ProtectedRoute>
-      <MyPostedGigs />
-    </ProtectedRoute>
-  }
-/>
-       
+          path="/bids/:gigId"
+          element={
+            <ProtectedRoute>
+              <Bids />
+            </ProtectedRoute>
+          }
+        />
 
+        <Route
+          path="/my-gigs"
+          element={
+            <ProtectedRoute>
+              <MyPostedGigs />
+            </ProtectedRoute>
+          }
+        />
 
-<Route
-  path="/my-bids"
-  element={
-    <ProtectedRoute>
-      <MyBids />
-    </ProtectedRoute>
-  }
-/>
+        <Route
+          path="/my-bids"
+          element={
+            <ProtectedRoute>
+              <MyBids />
+            </ProtectedRoute>
+          }
+        />
 
+        {/* Catch-all route */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-
-      
     </>
   );
 };
