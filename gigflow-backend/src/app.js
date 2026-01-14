06356f1883db4cfdc0eb8e2fa047774f4fd77709
surendrakumarreddy79@gmail.com <surendrayenika@gmail.com>
2026-01-14 14:56:07 +0530

    cors issues solved

--- gigflow-backend/src/app.js
+++ gigflow-backend/src/app.js
@@ -10,7 +10,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/gigs", gigRoutes);
